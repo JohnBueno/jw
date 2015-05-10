7c033152612ada16890a263ca902549c9f796d3f
John McCartney <jmm6026@bueno-3.local>
2015-05-10 19:28:38 -0400

    Home page work up

--- templates/content-home.php
+++ templates/content-home.php
@@ -1,4 +1,4 @@
-
+<?php use Roots\Sage\Nav\NavWalker; ?>
 <div class="row home">
 <?php if ($images = get_field("gallery")): ?>
 
@@ -10,9 +10,20 @@
 <div class="full-bg-container">
 	<div id="full-bg" class="full-bg"><img src="<?php echo($url); ?>"/></div>
 </div>
-
-
-
 <?php endif; ?>
+
+	<div class="col-sm-8 col-sm-offset-2 text-center hero">
+		<h1>JEN WOODRUFF</h1>
+		<div class="row">
+			<nav class="collapse navbar-collapse" role="navigation">
+				<?php
+				if (has_nav_menu('primary_navigation')) :
+				wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new NavWalker(), 'menu_class' => 'nav navbar-nav']);
+				endif;
+				?>
+			</nav>
+		</div>
+		<h2><em>Text/Value Prop</em></h2>
+	</div>
 
 </div>
--- templates/footer.php
+++ templates/footer.php
@@ -2,6 +2,14 @@
 <footer class="content-info row footer" role="contentinfo">
 	<div class="container">
 		<div class="row">
+			<div class="col-sm-4 col-sm-offset-4 text-center social">
+				<div class="row">
+					<div class="col-xs-4"><a href=""><i class="fa fa-facebook"></i></a></div>
+					<div class="col-xs-4"><a href=""><i class="fa fa-twitter"></i></a></div>
+					<div class="col-xs-4"><a href=""><i class="fa fa-instagram"></i></a></div>
+				</div>
+
+			</div>
 			<nav role="navigation">
 	            <?php
 			    	wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new NavWalker(), 'menu_class' => 'nav navbar-nav']);
